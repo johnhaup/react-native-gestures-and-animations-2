15506eaa4c84e293281a4a4b41b9785a300834ae
William Candillon <wcandillon@gmail.com>
2020-06-08 09:26:39 +0200

    :wrench:

--- src/D3/Label.tsx
+++ src/D3/Label.tsx
@@ -1,11 +1,22 @@
 import React from "react";
-import { View, Dimensions } from "react-native";
+import { View, Dimensions, StyleSheet } from "react-native";
 import { useDerivedValue, interpolate } from "react-native-reanimated";
 
 import { Vector } from "../components/AnimatedHelpers/Vector";
 import { ReText } from "../components/AnimatedHelpers";
+import { StyleGuide } from "../components";
 
 const { width } = Dimensions.get("window");
+const styles = StyleSheet.create({
+  date: {
+    ...StyleGuide.typography.title3,
+    textAlign: "center",
+  },
+  price: {
+    ...StyleGuide.typography.title2,
+    textAlign: "center",
+  },
+});
 const getDate = (data, domain, x) => {
   "worklet";
   const date = interpolate(x, [0, width], [domain.x[0], domain.x[1]]);
@@ -30,7 +41,7 @@ const formatMoney = (amount, decimalCount, decimal, thousands) => {
   decimalCount = isNaN(decimalCount) ? 2 : decimalCount;
   const negativeSign = amount < 0 ? "-" : "";
 
-  let i = `${Math.abs(amount).toFixed(decimalCount)}`;
+  let i = `${Math.abs(amount).toFixed(0)}`;
   let j = i.length > 3 ? i.length % 3 : 0;
 
   return (
@@ -49,11 +60,7 @@ const formatMoney = (amount, decimalCount, decimal, thousands) => {
 const Label = ({ domain, data, translate }: LabelProps) => {
   const date = useDerivedValue(() => {
     const d = getDate(data, domain, translate.x.value);
-    //const ye = new Intl.DateTimeFormat("en", { year: "numeric" }).format(d);
-    //const mo = new Intl.DateTimeFormat("en", { month: "short" }).format(d);
-    //const da = new Intl.DateTimeFormat("en", { day: "2-digit" }).format(d);
-    //console.log(`${da}-${mo}-${ye}`);
-    return d;
+    return d.toLocaleString();
   });
   const price = useDerivedValue(() => {
     const p = getPrice(data, domain, translate.y.value);
@@ -61,8 +68,8 @@ const Label = ({ domain, data, translate }: LabelProps) => {
   });
   return (
     <View>
-      <ReText text={date} />
-      <ReText text={price} />
+      <ReText style={styles.date} text={date} />
+      <ReText style={styles.price} text={price} />
     </View>
   );
 };
