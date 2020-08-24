e8325d63dc08608b972ede4126431f653e12b54f
William Candillon <wcandillon@gmail.com>
2020-08-24 12:42:39 +0200

    Custom animations 2 (#20)

--- src/components/AnimatedHelpers/Animations.ts
+++ src/components/AnimatedHelpers/Animations.ts
@@ -70,3 +70,77 @@ export function repeat(
     };
   });
 }
+
+interface Animation<State, PrevState = State> {
+  animation: (animation: State, now: number) => boolean;
+  current: number;
+  start: (
+    animation: State,
+    value: number,
+    now: number,
+    lastAnimation: PrevState
+  ) => void;
+}
+
+interface DecayAnimation extends Animation<DecayAnimation> {
+  lastTimestamp: number;
+  velocity: number;
+}
+
+interface WithBouncingDecayParams {
+  velocity: number;
+  deceleration?: number;
+  clamp: [number, number];
+}
+
+export const withBouncingDecay = ({
+  velocity: initialVelocity,
+  deceleration: userDeceleration,
+  clamp,
+}: WithBouncingDecayParams): number => {
+  "worklet";
+
+  const deceleration = userDeceleration ?? 0.998;
+  const VELOCITY_EPS = 5;
+  const decay = (animation: DecayAnimation, now: number) => {
+    const { lastTimestamp, current, velocity } = animation;
+    const dt = Math.min(now - lastTimestamp, 1000 / 16);
+
+    const kv = Math.pow(deceleration, dt);
+    const kx = (deceleration * (1 - kv)) / (1 - deceleration);
+
+    const v0 = velocity / 1000;
+    const v = v0 * kv * 1000;
+    const x = current + v0 * kx;
+
+    animation.lastTimestamp = now;
+    animation.current = x;
+    animation.velocity = v;
+
+    if (
+      (velocity < 0 && animation.current <= clamp[0]) ||
+      (velocity > 0 && animation.current >= clamp[1])
+    ) {
+      animation.current = clamp[velocity < 0 ? 0 : 1];
+      animation.velocity *= -0.5;
+    }
+
+    if (Math.abs(v) < VELOCITY_EPS) {
+      return true;
+    }
+    return false;
+  };
+
+  const start = (animation: DecayAnimation, value: number, now: number) => {
+    animation.current = value;
+    animation.lastTimestamp = now;
+    animation.velocity = initialVelocity;
+  };
+
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  return {
+    animation: decay,
+    start,
+  };
+};
